3ba5ebe9dba000bb726755b54c010b2152acdf30
Mathn <matheus.nascimento129@gmail.com>
2021-02-10 19:58:58 -0300

    Alteração nos Comentários.

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -13,19 +13,22 @@ package meuPrimeiroProgramaJava;
 import java.util.Scanner;
 
 /**
- * @author Mathn data: 03/02/2021
+ * @author Mathn 
+ * Data: 03/02/2021
  */
-public class Principal { // Classe que contém o método principal.
-	public static void main(String[] args) { // Este é o método principal.
+public class Principal { 
+	public static void main(String[] args) { 
+		
+		// Saldação e pergunta o nome.
 		System.out.println("Olá amigo!\nQual é o seu nome?");
 		
-		// Ler uma string do console usando a biblioteca Scanner.
+		// Ler o nome.
 		Scanner in =  new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		//Exibir um string usando printf.
+		// Saldação específica.
 		System.out.printf("Olá %s!", nome);
-		in.close();
+		
 	}
 
 }
